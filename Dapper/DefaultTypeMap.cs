1d5f86c71d90bf55e0560afc9d129ed11c4a6729
Kyle Wise <Kwise@pahousegop.com>
2024-11-01 09:16:31 -0400

    Added nuget package

--- Dapper/DefaultTypeMap.cs
+++ Dapper/DefaultTypeMap.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+
+//using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Reflection;
 
