bf1417052d96cb554daa0d648cca5056277eae38
Kyle Wise <Kwise@pahousegop.com>
2026-04-20 14:22:58 -0400

    encryption testing

---
+++ Dapper/EncryptedMetadataAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class EncryptedMetadataAttribute : Attribute
+    {
+        public DbType DataType { get; }
+        public int Size { get; }
+        public EncryptedMetadataAttribute(DbType type, int size) { DataType = type; Size = size; }
+    }
+
+}
